891ba2922966b6f7ab92d158edd71bbb0c8985f9
TrillesP <trillessky@gmail.com>
2023-02-07 17:33:45 -0300

    Wallet component rendering three other components

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import Header from '../components/Header';
 import Form from '../components/Form';
 import Expenses from '../components/Expenses';
 
-class Wallet extends Component{
+class Wallet extends React.Component{
     render() {
         return (
             <div>
@@ -16,4 +15,4 @@ class Wallet extends Component{
     }
 }
 
-export default connect()(Wallet);
+export default Wallet;
