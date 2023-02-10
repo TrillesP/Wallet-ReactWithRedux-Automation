583f9fa9dea263304714f74aadb28c5c7bc3509a
TrillesP <trillessky@gmail.com>
2023-02-10 12:13:38 -0300

    removed BrowserRouter for RTL

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './style.css';
 
 class App extends React.Component {
   render(){
     return (
-      <BrowserRouter>
         <Switch>
           <Route exact path='/' component={ Login }/>
           <Route exact path='/wallet' component={ Wallet }/>
         </Switch>
-      </BrowserRouter>
     );
   }
-  
 }
 
 export default App;
